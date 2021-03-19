044c7aa8905af55d72fe2e8b7e9a8666812d57ac
elu697 <elyou.dev@gmail.com>
2021-03-20 00:35:15 +0900

    Update init setting

--- WagesCounter/AppDelegate.swift
+++ WagesCounter/AppDelegate.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var window: UIWindow? = nil
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
